a8e04d78c59ccb76a93a02f46516284f7ab8deb6
Yan Cui <yancui@yans-mbp.mynet>
2017-06-07 00:29:44 +0100

    - don't use cron job on a per func basis - log when it's a coldstart

--- nodejs6-diff-package-size/no-deps/handler.js
+++ nodejs6-diff-package-size/no-deps/handler.js
@@ -1,6 +1,13 @@
 'use strict';
 
+let isColdstart = false;
+
 module.exports.handler = (event, context, callback) => {
+  if (!isColdstart) {
+    isColdstart = true;
+    console.log("this is a coldstart");
+  }
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
